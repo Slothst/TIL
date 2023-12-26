0a1777cdae9a3185b16fedb565f2d90f3181ac79
Slothst <cnj991279@gmail.com>
2023-12-26 18:01:07 +0900

    Stanford 강의 계산기 구현

---
+++ Swift/Stanford/Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,105 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by 최낙주 on 12/18/23.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    private var accumulator = 0.0
+    private var internalProgram = [AnyObject]()
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+        internalProgram.append(operand as AnyObject)
+    }
+    
+    func addUnaryOperation(symbol: String, operation: @escaping (Double) -> Double) {
+        operations[symbol] = Operation.UnaryOperation(operation)
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(Double.pi),
+        "e" : Operation.Constant(M_E),
+        "±" : Operation.UnaryOperation({ -$0 }),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({ $0 * $1 }),
+        "÷" : Operation.BinaryOperation({ $0 / $1 }),
+        "+" : Operation.BinaryOperation({ $0 + $1 }),
+        "−" : Operation.BinaryOperation({ $0 - $1 }),
+        "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        internalProgram.append(symbol as AnyObject)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value): 
+                accumulator = value
+            case .UnaryOperation(let function): 
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram as AnyObject
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
---
+++ Swift/Stanford/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 최낙주 on 12/18/23.
+//
+
+import UIKit
+
+var calculatorCount = 0
+
+class ViewController: UIViewController {
+    @IBOutlet private var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatorCount += 1
+        print("Loaded up a new Calculator (count = \(calculatorCount))")
+        brain.addUnaryOperation(symbol: "Z") { [ weak weakSelf = self ] in
+            weakSelf?.display.textColor = UIColor.red
+            return sqrt($0)
+        }
+    }
+    
+    deinit {
+        calculatorCount -= 1
+        print("Calculator left the heap (count = \(calculatorCount))")
+    }
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathmaticalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathmaticalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
